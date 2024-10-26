bd5f9ccef86e8134fbf83974fb4f10714b56b1a8
Jeffrey Dawes <jeffmcnallydawes@gmail.com>
2024-10-26 07:10:35 -0700

    Add error case unit tests

--- app/src/test/java/com/justjeff/graphqlexample/MainViewModelTest.kt
+++ app/src/test/java/com/justjeff/graphqlexample/MainViewModelTest.kt
@@ -3,7 +3,9 @@ package com.justjeff.graphqlexample
 import app.cash.turbine.test
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.annotations.ApolloExperimental
+import com.apollographql.apollo.api.Error
 import com.apollographql.apollo.testing.QueueTestNetworkTransport
+import com.apollographql.apollo.testing.enqueueTestNetworkError
 import com.apollographql.apollo.testing.enqueueTestResponse
 import com.justjeff.graphqlexample.models.RepositoryQuery
 import kotlinx.coroutines.test.runTest
@@ -21,16 +23,48 @@ class MainViewModelTest {
         .build()
     private val subject = MainViewModel(client)
 
+    private val query = RepositoryQuery("github-react", "jeffmcnd")
+
     @Test
     fun `initialize - Fetches description`() = runTest(dispatcherRule.dispatcher) {
-        val query = RepositoryQuery("github-react", "jeffmcnd")
         val data = RepositoryQuery.Data(RepositoryQuery.Repository("description"))
-        client.enqueueTestResponse(query, data)
-
+        client.enqueueTestResponse(query, data, errors = null)
         subject.state.test {
             Assert.assertEquals(MainUiState("Loading..."), awaitItem())
             subject.initialize()
             Assert.assertEquals(MainUiState("description"), awaitItem())
+        }
+    }
+
+    @Test
+    fun `initialize - Error sets text to first message`() = runTest(dispatcherRule.dispatcher) {
+        val message = "No repository found."
+        val errors = listOf(Error.Builder(message).build())
+        client.enqueueTestResponse(operation = query, data = null, errors = errors)
+        subject.state.test {
+            Assert.assertEquals(MainUiState("Loading..."), awaitItem())
+            subject.initialize()
+            Assert.assertEquals(MainUiState(message), awaitItem())
+        }
+    }
+
+    @Test
+    fun `initialize - Null data and error sets text`() = runTest(dispatcherRule.dispatcher) {
+        client.enqueueTestResponse(query, null, null)
+        subject.state.test {
+            Assert.assertEquals(MainUiState("Loading..."), awaitItem())
+            subject.initialize()
+            Assert.assertEquals(MainUiState("Unexpected error. Try again."), awaitItem())
+        }
+    }
+
+    @Test
+    fun `initialize - Network error sets text`() = runTest(dispatcherRule.dispatcher) {
+        client.enqueueTestNetworkError()
+        subject.state.test {
+            Assert.assertEquals(MainUiState("Loading..."), awaitItem())
+            subject.initialize()
+            Assert.assertEquals(MainUiState("Unexpected error. Try again."), awaitItem())
         }
     }
 }
